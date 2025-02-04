a59eafaca5d6393b53f2c489dcec297d5d3fe0d6
einzi-0 <einzicode@gmail.com>
2025-02-04 18:15:22 +0530

    update

---
+++ src/lib/appwrite.ts
@@ -0,0 +1,13 @@
+// src/lib/server/appwrite.js
+"use server";
+import sdk from "node-appwrite";
+import { cookies } from "next/headers";
+
+export const client = new sdk.Client();
+
+client
+    .setEndpoint("https://cloud.endermanbot.me/v1")
+    .setProject(process.env.PROJECT)
+    .setKey(process.env.APPWRITE_KEY);
+
+export const storage = new sdk.Storage(client);
